7ab0eff3332fcfca8ea126528253b67fc76f03ac
fatihozkul35 <fatihozkul3541@gmail.com>
2022-03-13 10:51:47 +0100

    version 1.0

---
+++ src/components/Square.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+const style = {
+  background: "lightblue",
+  border: "2px solid darkblue",
+  fontSize: "30px",
+  fontWeight: "800",
+  cursor: "pointer",
+  outline: "none",
+};
+const Square = ({ onClick, value }) => (
+  <button style={style} onClick={onClick}>
+    {value}
+  </button>
+);
+
+export default Square;
